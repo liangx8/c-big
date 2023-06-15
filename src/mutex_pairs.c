9edc2db159c77708b7b7988dadfce8937e23aa1b
boku <mra@Precision-T3600.com>
2023-06-15 18:51:55 +0800

    设计子线程

--- src/mutex_pairs.c
+++ src/mutex_pairs.c
@@ -1,7 +1,6 @@
 /**
- * - 从空的容器中取对象会阻塞
- * - 有一个封条状态，在此状态下，容器不能在压入对象，但可以取出对象。封条状态不能取消
-*/
+ * 非阻塞的容器
+ */
 
 #include <stdint.h>
 #include <malloc.h>
@@ -9,7 +8,6 @@
 #include <assert.h>
 #include "error_stack.h"
 #define MAX 1000
-#define SEALING -2
 struct Pairs
 {
     int64_t data[MAX * 2];
@@ -21,30 +19,22 @@ struct Pairs
 
 struct MutexPairs
 {
-    pthread_mutex_t pm; // producer mutex，必须写成实例对象，在建立的时候不用特意用另外3次malloc
-    pthread_mutex_t cm; // consumer mutex
-    pthread_cond_t cond;
+    // 非阻塞式的容器，应该只需要１个排他锁
+    pthread_mutex_t mutex;
     struct Pairs *head;
-    // 当seal 为真,就put不再接受任何数据,get不会阻塞
-    uint64_t seal;
 };
 
 struct MutexPairs *mutex_create(void)
 {
     struct MutexPairs *mp = malloc(sizeof(struct MutexPairs));
-    pthread_mutex_init(&mp->pm, NULL);
-    pthread_mutex_init(&mp->cm, NULL);
-    pthread_cond_init(&mp->cond, NULL);
+    pthread_mutex_init(&mp->mutex, NULL);
 
     mp->head=malloc(sizeof(struct Pairs));
     mp->head->idx=0;
     mp->head->next=NULL;
-
-    mp->seal=0;
-
     return mp;
 }
-struct MutexPairs *mutex_with_array(pthread_mutex_t *mutex, const int64_t *data, int size)
+struct MutexPairs *mutex_with_array(const int64_t *data, int size)
 {
     assert((size & 1) == 0);
     struct MutexPairs *mp = mutex_create();
@@ -83,9 +73,7 @@ struct MutexPairs *mutex_with_array(pthread_mutex_t *mutex, const int64_t *data,
 void pairs_free(struct Pairs *);
 void mutex_free(struct MutexPairs *mp)
 {
-    pthread_mutex_destroy(&mp->pm);
-    pthread_mutex_destroy(&mp->cm);
-    pthread_cond_destroy(&mp->cond);
+    pthread_mutex_destroy(&mp->mutex);
     pairs_free(mp->head);
     free(mp);
 }
@@ -94,72 +82,44 @@ void mutex_free(struct MutexPairs *mp)
  * @brief 获取一个数据
  * @param ps 数据对容器
  * @param ref 如果容器无内容, 堵塞
- * @return 出错返回-1，否则 0
+ * @return 出错返回-1,空容器返回 1,否则 0
  */
 int mutex_get(struct MutexPairs *mps, int64_t **ref)
 {
 
-    if (pthread_mutex_lock(&mps->cm))
+    if (pthread_mutex_lock(&mps->mutex))
     {
         ERROR("锁错误");
         return -1;
     }
-    while (1)
+    struct Pairs *ptr = mps->head;
+    while (ptr)
     {
-        struct Pairs *ptr = mps->head;
-        while (ptr)
+        if (ptr->idx)
         {
-            if (ptr->idx)
-            {
-                int idx;
-                ptr->idx--;
-                idx = ptr->idx;
-                *ref = &ptr->data[idx * 2];
-                if (pthread_mutex_unlock(&mps->cm))
-                {
-                    ERROR("解锁锁错误");
-                    return -1;
-                }
-                return 0;
-            }
-            ptr = ptr->next;
-        }
-        // 没有数据,当前线程等待 条件信号
-        if(mps->seal != 0){
-            if (pthread_mutex_unlock(&mps->cm))
+            int idx;
+            ptr->idx--;
+            idx = ptr->idx;
+            *ref = &ptr->data[idx * 2];
+            if (pthread_mutex_unlock(&mps->mutex))
             {
                 ERROR("解锁锁错误");
                 return -1;
             }
-            return SEALING;
+            return 0;
         }
-        pthread_cond_wait(&mps->cond, &mps->cm);
-        if(mps->seal != 0){
-            if (pthread_mutex_unlock(&mps->cm))
-            {
-                ERROR("解锁锁错误");
-                return -1;
-            }
-            return SEALING;
-        }
+        ptr = ptr->next;
     }
+    if (pthread_mutex_unlock(&mps->mutex))
+    {
+        ERROR("解锁锁错误");
+        return -1;
+    }
+    return 1;
 }
 int mutex_put(struct MutexPairs *mps, int64_t p1, int64_t p2)
 {
-    if (pthread_mutex_lock(&mps->pm))
-    {
-        ERROR("锁错误");
-        return -1;
-    }
-    if (mps->seal){
-        if (pthread_mutex_unlock(&mps->pm))
-        {
-            ERROR("解锁锁错误");
-            return -1;
-        }
-        return SEALING;
-    }
-    if (pthread_mutex_lock(&mps->cm))
+    if (pthread_mutex_lock(&mps->mutex))
     {
         ERROR("锁错误");
         return -1;
@@ -174,6 +134,7 @@ int mutex_put(struct MutexPairs *mps, int64_t p1, int64_t p2)
                 ptr->next = malloc(sizeof(struct Pairs));
             }
             ptr = ptr->next;
+            ptr->idx=0;
         }
         else
         {
@@ -181,16 +142,7 @@ int mutex_put(struct MutexPairs *mps, int64_t p1, int64_t p2)
             ptr->idx++;
             ptr->data[idx] = p1;
             ptr->data[idx + 1] = p2;
-            if(pthread_cond_signal(&mps->cond)){
-                ERROR("条件信号错误");
-                return -1;
-            }
-            if (pthread_mutex_unlock(&mps->cm))
-            {
-                ERROR("解锁锁错误");
-                return -1;
-            }
-            if (pthread_mutex_unlock(&mps->pm))
+            if (pthread_mutex_unlock(&mps->mutex))
             {
                 ERROR("解锁锁错误");
                 return -1;
@@ -202,119 +154,144 @@ int mutex_put(struct MutexPairs *mps, int64_t p1, int64_t p2)
 void pairs_print(struct Pairs *ps, FILE *out, int limit);
 int mutex_print(struct MutexPairs *mps, FILE *out, int limit)
 {
-    if (pthread_mutex_lock(&mps->cm))
+    if (pthread_mutex_lock(&mps->mutex))
     {
         ERROR("锁错误");
         return -1;
     }
     pairs_print(mps->head, out, limit);
-    if (pthread_mutex_unlock(&mps->cm))
+    if (pthread_mutex_unlock(&mps->mutex))
     {
         ERROR("解锁锁错误");
         return -1;
     }
     return 0;
 }
-/**
- * @brief 对象mps不再接受数据，如果对象已经空，mutex_get()不再阻塞，并且返回数据为空
- * @param mps 对象
- * @return 出错返回非零值
-*/
-int mutex_seal(struct MutexPairs *mps){
-    // 只需要生产者锁(pm),
-    if (pthread_mutex_lock(&mps->pm))
-    {
-        ERROR("解锁锁错误");
-        return -1;
-    }
-    mps->seal=1;
-    if(pthread_cond_broadcast(&mps->cond)){
-        ERROR("条件信号错误");
-        return -1;
-    }
-    if (pthread_mutex_unlock(&mps->pm))
-    {
-        ERROR("解锁锁错误");
-        return -1;
-    }
-    return 0;
-}
-// unit test ------------------------------
 extern struct timespec MS1;
+// unit test 1------------------------------
+// 简单的４进程５次循环测试
+const struct timespec NS100={0,10000000};
 
 struct test_struct{
     struct MutexPairs *mp;
-    int id;
-    int sem;
-    pthread_mutex_t *mutex;
+    int64_t id;
 };
 void *test_pair_child(void *obj)
 {
     struct test_struct *ts=obj;
-    int res;
-    int64_t *ptr;
-    pthread_mutex_lock(ts->mutex);
-    ts->sem++;
-    pthread_mutex_unlock(ts->mutex);
-    while(1){
-        int64_t left,right,mid;
-
-        pthread_mutex_lock(ts->mutex);
-        ts->sem--;
-        pthread_mutex_unlock(ts->mutex);
-
-        res=mutex_get(ts->mp,&ptr);
-        if(res==SEALING){
-            break;
-        }
-        if(res){
-            return (void *)-1;
-        }
-
-        pthread_mutex_lock(ts->mutex);
-        ts->sem++;
-        pthread_mutex_unlock(ts->mutex);
-
-        left=ptr[0];
-        right=ptr[1];
-        if(left + 1==right){
-
-            continue;
-        }
-
+    for(long lx=0;lx<5;lx++){
+        mutex_put(ts->mp,ts->id,lx);
+        nanosleep(&MS1,NULL);
     }
 
     return NULL;
 }
-void test_pair(void)
-{
+void unit_test1(void){
+    pthread_t id[4];
     struct MutexPairs *mp=mutex_create();
-    pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
-    int64_t *ptr;
-    pthread_t tid[4];
-    mutex_put(mp,0,100000);
-    for(int ix=0;ix<3;ix++){
-        struct test_struct *ts=malloc(sizeof(struct test_struct));
-        ts->id=ix;
-        ts->mp=mp;
-        ts->mutex=&mutex;
-        pthread_create(&tid[ix],NULL,test_pair_child,ts);
+    int64_t *pd;
+    for(int px=0;px<4;px++){
+        struct test_struct *pts=malloc(sizeof(struct test_struct));
+        pts->mp=mp;
+        pts->id=px;
+        pthread_create(&id[px],NULL,test_pair_child,pts);
     }
-
     for(int ix=0;ix<20;ix++){
-        if(mutex_get(mp,&ptr)) {
-            print_error_stack(stdout);
-            break;
+        while(1){
+            int res=mutex_get(mp,&pd);
+            if(res==-1){
+                printf("出错了\n");
+                return;
+            }
+            if(res==0){
+                break;
+            }
         }
-        printf("%2d:%ld,%ld\n",ix,ptr[0],ptr[1]);
+        printf("id:%4ld num:%4ld\n",pd[0],pd[1]);
     }
     for(int ix=0;ix<4;ix++){
-        void *retval;
-        pthread_join(tid[ix],&retval);
-        if(retval){
-            print_error_stack(stdout);
-            break;
-        }
+        pthread_join(id[ix],NULL);
     }
 
+}
+// unit test 2 ---------------------------------------
+// 类似快速排序的分区测试
+struct test2_data{
+    struct MutexPairs *mp;
+    pthread_mutex_t   *mutex;
+    //线程之间都是满负荷，容器中空的可能性比较低，只在开始和最后才会出现
+    pthread_cond_t    *cond;
+    int               *tnum;
+    int64_t           id;
+};
+void *test2_child(void *obj){
+    // 设计一个空闲标记，当检测到容器中没有新的数据，进入空闲状态并且tnum减一
+    // 进入pthread_cond_wait()
+    // 当容器中取得数据，并且是空闲状态取消空闲状态，tnum加一
+    struct test2_data *t2d=obj;
+    int64_t *pair;
+    int idle=1;
+    while(1){
+        pthread_mutex_lock(t2d->mutex);
+        int res=mutex_get(t2d->mp,&pair);
+        if(res==-1){
+            return NULL;
+        }
+        if(res==1){
+            // container is empty,此时tnum一定大于0
+            int tnum=*(t2d->tnum);
+            *(t2d->tnum)=tnum-1;
+            idle=1;
+            pthread_cond_wait(t2d->cond,t2d->mutex);
+            pthread_mutex_unlock(t2d->mutex);
+            continue;
+        }
+        if(idle){
+            int tnum;
+            idle=0;
+            tnum=*(t2d->tnum);
+            *(t2d->tnum)=tnum+1;
+        }
+        pthread_mutex_unlock(t2d->mutex);
+
+        int64_t left=pair[0];
+        int64_t right=pair[1];
+        int64_t mid=(right+left)/2;
+        nanosleep(&NS100,NULL);
+        pthread_mutex_lock(t2d->mutex);
+        if(mid-left>2){
+            mutex_put(t2d->mp,left,mid);
+            pthread_cond_signal(t2d->cond);
+        }
+        if(right-mid>2){
+            mutex_put(t2d->mp,mid,right);
+            pthread_cond_signal(t2d->cond);
+        }
+        pthread_mutex_unlock(t2d->mutex);
+    }
+    return NULL;
+}
+const int64_t init_data[]={0,400,400,1000};
+void unit_test2(void)
+{
+    struct MutexPairs *mp=mutex_with_array(init_data,4);
+    pthread_cond_t cond=PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+    int sem;
+    pthread_t pid[4];
+    sem=0;
+    for(int ix=0;ix<4;ix++){
+        struct test2_data *t2d=malloc(sizeof(struct test2_data));
+        t2d->cond=&cond;
+        t2d->mutex=&mutex;
+        t2d->tnum=&sem;
+        t2d->id=ix;
+        t2d->mp=mp;
+        pthread_create(&pid[ix],NULL,test2_child,t2d);
+    }
+
+}
+void test_pair(void)
+{
+    unit_test1();
 }
